6990904b82fc030b3b35d11a2b5f540f054f0bb9
Marcin Lawniczak <marcin@lawniczak.me>
2018-11-20 18:23:13 +0100

    Show event attendees

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::prefix('events')->middleware(['auth'])->group(function () {
     //List all events
     Route::get('/', 'EventsController@getList')->name('events.list');
     //Show single event
-    Route::get('{id}', 'EventsController@getSingle');
+    Route::get('{Event}', 'EventsController@getSingle');
     //List user events
     Route::get('/attended', 'EventsController@getUserList')->name('events.attended');
     //Show single event
