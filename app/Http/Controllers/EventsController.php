dc00be55cfa4be8dc556525c466965f43dc4c1a9
Marcin Lawniczak <marcin@lawniczak.me>
2018-11-20 17:53:00 +0100

    Add attendees count

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -10,7 +10,7 @@ use App\Event;
 class EventsController extends Controller
 {
     public function getList() {
-        $events = Event::withCount('users');
+        $events = Event::all()->withCount('users');
         return view('events.list')->withEvents($events);
     }
 
